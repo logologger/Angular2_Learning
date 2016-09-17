9ba7f91a18734ac67cd3099009e7c7722f11b277
razzcoder <rajatsbhadauria@gmail.com>
2016-09-17 23:31:48 +0530

    Lecture 9

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,13 +1,29 @@
 import {Component} from 'angular2/core';
 import {Config} from './config.service';
+import {Video} from './video';
+import {PlayListComponenet} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{mainHeading}}</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives:[PlayListComponenet]
 })
 
 export class AppComponent {
 mainHeading=Config.MAINHEADING;
+videos:Array<Video>;
+
+
+constructor(){
+
+	this.videos=[
+	new Video(1,"Rajat Singh","f8qBeaGe2S4","About Angular 2"),
+	new Video(2,"Rajat Singh","f8qBeaGe2S4","About Angular 2"),
+	new Video(3,"Rajat Singh","f8qBeaGe2S4","About Angular 2"),
+	new Video(4,"Rajat Singh","f8qBeaGe2S4","About Angular 2"),
+
+	];
+}
 
 }  
 /* make the class public  */
--- app/ts/playlist.component.ts
+++ app/ts/playlist.component.ts
@@ -1,8 +1,16 @@
 import {Component} from 'angular2/core';
+import {Video} from './video';
 
 @Component({
     selector:'playlist',
-    templateUrl:'app/ts/playlist.component.html'
+    templateUrl:'app/ts/playlist.component.html',
+    inputs:['videos']
 })
 
-export class PlayListComponenet{}
+export class PlayListComponenet{
+
+
+	onSelect(vid:Video){
+		console.log(JSON.stringify(vid));
+	}
+}
